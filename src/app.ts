5c033856f735a8f952fff0006898bb628b6c694a
kalungirasuli <87176642+kalungirasuli@users.noreply.github.com>
2025-07-10 16:01:06 +0300

    Revert "Retry the previous action"

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,6 @@
 import express, { Request, Response } from 'express'
-import authRouter from './routes/auth';
-
 const app = express()
 const port = 3000
-
-app.use(express.json());
-app.use('/api/auth', authRouter);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!')
--- src/routes/auth.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { register, verifyEmail } from '../controllers/authController';
-
-const router = Router();
-
-router.post('/register', register);
-router.get('/verify-email', verifyEmail);
-
-export default router;
